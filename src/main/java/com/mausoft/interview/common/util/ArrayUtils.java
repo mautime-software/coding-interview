b7e5f19aff1b55d30bbb3a1711a8c2f0015644ae
mautime <mau.time.software@gmail.com>
2024-02-06 14:15:06 -0600

    Utils - Updated [ArrayUtils] to support swapping values in a 2D array

--- src/main/java/com/mausoft/interview/common/util/ArrayUtils.java
+++ src/main/java/com/mausoft/interview/common/util/ArrayUtils.java
@@ -7,6 +7,12 @@ public class ArrayUtils {
         nums[b] = tmp;
     }
 
+    public static void swap(int[][] nums, int[] a, int[] b) {
+        int tmp = nums[a[0]][a[1]];
+        nums[a[0]][a[1]] = nums[b[0]][b[1]];
+        nums[b[0]][b[1]] = tmp;
+    }
+
     public static int[] copy(int[] nums, int i, int j) {
         int[] copyArr = new int[j - i + 1];
         for (int x = 0; x < copyArr.length; x++) {
